2c8b512e2128b2f63f74df807c1579339c69bc59
Фыр <danil.erlih@gmail.com>
2026-03-04 18:01:37 +0800

    average #1

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-user_input_square = int(input("Введите число, чтобы возвести его в квадрат: "))
+# 1
+user_input_square = int(input('Введите число, чтобы возвести его в квадрат: '))
 square_result = user_input_square**2
-print(square_result)
-
-
-
+print(f'{user_input_square} в квадрате = {square_result}')
+print()
+# 2
+user_input_average = list(input('Введите трех значное число: '))
+user_input_average[0] = int(user_input_average[0])
+user_input_average[1] = int(user_input_average[1])
+user_input_average[2] = int(user_input_average[2])
+average_result = int(user_input_average[0] + user_input_average[1] + user_input_average[2]) / 3
+print(f'Среднее число из числел {user_input_average[0]}, {user_input_average[1]}, {user_input_average[2]} = {average_result}')
+print()
 
 
 
