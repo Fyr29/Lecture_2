6ea3ff0dd2eb8def99dc0a441dee465190e23247
Фыр <danil.erlih@gmail.com>
2026-03-04 19:28:53 +0800

    GitHub

--- main.py
+++ main.py
@@ -87,5 +87,5 @@ print(f'И второй столбик\n{user_input_number_2[0]}\n{user_input_nu
 # git add README.md
 # git commit -m "first commit"
 # git branch -M main
-# git remote add origin https://github.com/Fyr29/education_python.git
+# git remote add origin https://github.com/Fyr29/Lecture_2.git
 # git push -u origin main
